959b467e411db44e0c00ad39bf8e573555226b92
Aeva Ntsc <aeva.ntsc@gmail.com>
2012-11-12 03:12:53 -0600

    Updated build script.

--- libvcm/build_debug.sh
+++ libvcm/build_debug.sh
@@ -1,1 +1,1 @@
-valac debug.vala data_block.vala raster_layer.vala simple_voxel.vala interfaces.vala -X -lm -g --save-temps -o debug.out
+valac debug.vala voxel_model.vala data_block.vala raster_layer.vala simple_voxel.vala interfaces.vala --pkg gee-1.0 -X -lm -g --save-temps -o debug.out --target-glib=2.32
