c990948fcb53e5b3ccd24877f75b217eb3608cad
Aeva Ntsc <aeva.ntsc@gmail.com>
2012-11-12 01:58:56 -0600

    updated debug build script

--- libvcm/build_debug.sh
+++ libvcm/build_debug.sh
@@ -1,1 +1,1 @@
-valac debug.vala data_block.vala raster_layer.vala simple_voxel.vala interfaces.vala -X -lm -g --save-temps
+valac debug.vala data_block.vala raster_layer.vala simple_voxel.vala interfaces.vala -X -lm -g --save-temps -o debug.out
